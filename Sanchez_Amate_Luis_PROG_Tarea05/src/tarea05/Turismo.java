3b50666b1511cdb21afa63dc76fcf8bcef320f84
LUIS-PC <LUIS-PC@LUIS>
2018-01-26 13:34:49 +0100

    Creamos el constructor de Turismo que comprueba matriculas y cilindradas

--- Sanchez_Amate_Luis_PROG_Tarea05/src/tarea05/Turismo.java
+++ Sanchez_Amate_Luis_PROG_Tarea05/src/tarea05/Turismo.java
@@ -1,13 +1,42 @@
 
 package tarea05;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Turismo {
     
-    public String Matricula;
+    public String matricula;
     public String marca;
     public String modelo;
     public int cilindrada;
     public boolean disponible;
     
+    
+    public Turismo(String matricula, String marca, String modelo, int cilindrada){
+        
+        if (compruebaMatricula(matricula))
+		this.matricula = matricula;
+	else
+		throw new ExcepcionAlquilerVehiculos("Matrícula no válida");
+        
+        this.marca=marca;
+        this.modelo=modelo;
+        
+        if (cilindrada>0)
+            this.cilindrada=cilindrada;
+        else
+            throw new ExcepcionAlquilerVehiculos("La cilindrada no puede ser menor o igual a 0");
+        
+        disponible=true;
+        
+    }
+    
+    private boolean compruebaMatricula(String matricula) {
+		Pattern patron = Pattern.compile("[0-9]{4}[B-DF-HJ-NP-TV-Z]{3}");
+		Matcher emparejador = patron.matcher(matricula);
+		return emparejador.matches();
+    }
+                
     
 }
