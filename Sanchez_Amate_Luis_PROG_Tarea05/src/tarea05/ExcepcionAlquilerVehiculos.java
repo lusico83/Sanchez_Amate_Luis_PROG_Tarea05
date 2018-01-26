08f2377f5204858dcec91b0877b49c578a0e00b5
LUIS-PC <LUIS-PC@LUIS>
2018-01-26 12:34:02 +0100

    Creamos la clase de excepcion que cogerá como parametro un mensaje de error

---
+++ Sanchez_Amate_Luis_PROG_Tarea05/src/tarea05/ExcepcionAlquilerVehiculos.java
@@ -0,0 +1,13 @@
+
+package tarea05;
+
+public class ExcepcionAlquilerVehiculos extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ExcepcionAlquilerVehiculos(String mensaje) {
+		super(mensaje);
+	}
+    
+    
+}
