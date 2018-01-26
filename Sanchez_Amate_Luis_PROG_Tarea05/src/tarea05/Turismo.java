a2cd1a1a84120a37754896f92ffdbd56fb8cd8ac
LUIS-PC <LUIS-PC@LUIS>
2018-01-26 13:15:55 +0100

    Creamos la clase Turismo

---
+++ Sanchez_Amate_Luis_PROG_Tarea05/src/tarea05/Turismo.java
@@ -0,0 +1,13 @@
+
+package tarea05;
+
+public class Turismo {
+    
+    public String Matricula;
+    public String marca;
+    public String modelo;
+    public int cilindrada;
+    public boolean disponible;
+    
+    
+}
