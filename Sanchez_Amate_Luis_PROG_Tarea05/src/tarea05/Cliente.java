9004092915bee6d37e6f06ef58beca28f4c386b3
LUIS-PC <LUIS-PC@LUIS>
2018-01-26 13:07:01 +0100

    Creamos el constructor con parametros y el constructor copia de la clase clientes, asi como los metodos get

--- Sanchez_Amate_Luis_PROG_Tarea05/src/tarea05/Cliente.java
+++ Sanchez_Amate_Luis_PROG_Tarea05/src/tarea05/Cliente.java
@@ -14,5 +14,78 @@ public class Cliente {
     private int identificador;
     private static int numClientes=0;
     
-  
+  public Cliente(Cliente cliente){
+        
+        nombre=cliente.getNombre();
+        dni=cliente.getDni();
+        direccion=cliente.getDireccion();
+        localidad=cliente.getLocalidad();
+        codigoPostal=cliente.getCodigoPostal();
+        identificador=cliente.getIdentificador();        
+        
+    }
+    
+    public Cliente(String nombre, String dni, String direccion, String localidad, String codigoPostal){
+        
+        this.nombre=nombre;
+        
+        if (compruebaDni(dni))
+            this.dni=dni;
+        else
+            throw new ExcepcionAlquilerVehiculos("DNI no válido");
+        
+        
+        this.direccion=direccion;
+        this.localidad=localidad;
+        
+        if (compruebaCodigoPostal(codigoPostal))
+            this.codigoPostal=codigoPostal;
+        else
+            throw new ExcepcionAlquilerVehiculos("Codigo Postal no válido");
+        
+        numClientes=numClientes+1;
+        identificador=numClientes;
+        
+    }
+        
+        private boolean compruebaDni(String dni) {
+		Pattern patron = Pattern.compile("[0-9]{8}[A-Z]");
+		Matcher emparejador = patron.matcher(dni);
+		return emparejador.matches();
+	}
+	
+	private boolean compruebaCodigoPostal(String codigoPostal) {
+		Pattern patron = Pattern.compile("[0-9]{5}");
+		Matcher emparejador = patron.matcher(codigoPostal);
+		return emparejador.matches();
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public String getDni() {
+		return dni;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+	
+	public String getLocalidad() {
+		return localidad;
+	}
+	
+	public String getCodigoPostal() {
+		return codigoPostal;
+	}
+	
+	public int getIdentificador() {
+		return identificador;
+	}    
+        
+        
+        
+    
+      
 }
