3bff3128b9d7493922745077ca2588e65ee12fd7
LUIS-PC <LUIS-PC@LUIS>
2018-01-30 16:47:48 +0100

    Clase AlquilerVehiculos y sus atributos

---
+++ Sanchez_Amate_Luis_PROG_Tarea05/src/tarea05/AlquilerVehiculos.java
@@ -0,0 +1,14 @@
+
+package tarea05;
+
+
+public class AlquilerVehiculos {
+    
+    private Cliente[] clientes;
+    private Turismo[] turismos;
+    private Alquiler[] alquileres;
+    private final int MAX_TURISMOS = 20;
+    private final int MAX_CLIENTES = 20;
+    private final int MAX_ALQUILERES = 20;
+    
+}
