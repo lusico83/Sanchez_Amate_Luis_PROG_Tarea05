0bb51a9e10369bcb990416c8cfe4e5e8669adc57
Luis Sánchez Amate <lois_@DESKTOP-6I7TUU9>
2018-01-30 15:28:08 +0100

    Creacion de la clase Alquiler y sus atributos

---
+++ Sanchez_Amate_Luis_PROG_Tarea05/src/tarea05/Alquiler.java
@@ -0,0 +1,18 @@
+
+package tarea05;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Alquiler {
+    
+    private Cliente cliente;
+    private Turismo turismo;
+    private Date fecha;
+    private int dias;
+    private final SimpleDateFormat FORMATO_FECHA= new SimpleDateFormat("dd/MM/yyyy HH:mm");
+    private final double PRECIO_DIA = 30.0;
+    
+    
+    
+}
