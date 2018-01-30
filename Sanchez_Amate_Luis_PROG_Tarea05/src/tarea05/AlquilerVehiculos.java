64e97c35bed56280de59eb0cb872f5e915e3f114
LUIS-PC <LUIS-PC@LUIS>
2018-01-30 17:00:36 +0100

    Metodo getCliente que devuelve un cliente o null depende si coincide o no con el dni que introducimos

--- Sanchez_Amate_Luis_PROG_Tarea05/src/tarea05/AlquilerVehiculos.java
+++ Sanchez_Amate_Luis_PROG_Tarea05/src/tarea05/AlquilerVehiculos.java
@@ -1,6 +1,8 @@
 
 package tarea05;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AlquilerVehiculos {
     
@@ -13,9 +15,24 @@ public class AlquilerVehiculos {
     
     
     public AlquilerVehiculos() {
-		clientes = new Cliente[MAX_CLIENTES];
-		turismos = new Turismo[MAX_TURISMOS];
-		alquileres = new Alquiler[MAX_ALQUILERES];
+	clientes = new Cliente[MAX_CLIENTES];
+	turismos = new Turismo[MAX_TURISMOS];
+	alquileres = new Alquiler[MAX_ALQUILERES];
+    }
+    
+    public Cliente getCliente(String dni){
+        for (int i=0;i<=MAX_CLIENTES;i++){
+            if (compruebaDni(dni))
+                return clientes[i];
+            else
+                return null;  
+        }
+    }
+    
+    private boolean compruebaDni(String dni) {
+		Pattern patron = Pattern.compile("[0-9]{8}[A-Z]");
+		Matcher emparejador = patron.matcher(dni);
+		return emparejador.matches();
 	}
     
 }
